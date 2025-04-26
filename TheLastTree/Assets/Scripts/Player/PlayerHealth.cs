afc135471d1620d747df86b5c59128a8f1672d06
troublemake3r <m.a.dukaczewska@gmail.com>
2025-04-26 16:39:10 +0200

    Added player health bar functionality

--- TheLastTree/Assets/Scripts/Player/PlayerHealth.cs
+++ TheLastTree/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
     public int currentPlayerHealth;
+
+    [SerializeField] private Image healthFillImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +18,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentPlayerHealth -= amount;
         currentPlayerHealth = Mathf.Max(currentPlayerHealth, 0);
+        UpdateHealthBar();
         if (currentPlayerHealth <= 0)
         {
             Die();
@@ -25,6 +29,13 @@ public class PlayerHealth : MonoBehaviour
     {
         currentPlayerHealth += amount;
         currentPlayerHealth = Mathf.Min(currentPlayerHealth, maxHealth);
+    }
+    private void UpdateHealthBar()
+    {
+        if (healthFillImage != null)
+        {
+            healthFillImage.fillAmount = (float)currentPlayerHealth / maxHealth;
+        }
     }
 
     void Die()
